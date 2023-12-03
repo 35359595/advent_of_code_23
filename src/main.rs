c71e36ed2e0543779810faf7dfe86e0cccc2f463
Ivan Temchenko <35359595i@proton.me>
2023-12-02 21:47:06 -0800

    day 3 both

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code, unused_assignments, unused_imports, unused_variables)]
 use core::panic;
+use std::collections::btree_map::Range;
 use std::collections::vec_deque::VecDeque;
 use std::collections::{HashMap, HashSet};
 use std::env::args;
@@ -139,8 +140,68 @@ fn day_2() {
 }
 
 fn day_3() {
-    println!("Result for day 3: {}", 0);
     let inputs = read_lines(3);
+    fn consider_number_neighbors(
+        board: &Vec<String>,
+        gear_nums: &mut HashMap<(usize, usize), Vec<i32>>,
+        start_y: usize,
+        start_x: usize,
+        end_y: usize,
+        end_x: usize,
+        num: i32,
+    ) -> bool {
+        for y in start_y..=end_y {
+            for x in start_x..=end_x {
+                if y < board.len() && x < board[y].len() {
+                    if !board[y].chars().nth(x).unwrap().is_numeric()
+                        && board[y].chars().nth(x).unwrap() != '.'
+                    {
+                        if board[y].chars().nth(x).unwrap() == '*' {
+                            gear_nums.entry((y, x)).or_insert(vec![]).push(num);
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        false
+    }
+    let mut total = 0;
+    let mut board = Vec::new();
+    let mut gear_nums = HashMap::new();
+
+    let num_pattern = fancy_regex::Regex::new(r"\d+").unwrap();
+
+    for line in inputs.filter_map(|l| l.ok()) {
+        board.push(line.trim().to_string());
+    }
+
+    for (row_num, line) in board.iter().enumerate() {
+        for mat in num_pattern.find_iter(line).filter_map(|m| m.ok()) {
+            if consider_number_neighbors(
+                &board,
+                &mut gear_nums,
+                row_num.saturating_sub(1),
+                mat.start().saturating_sub(1),
+                row_num + 1,
+                mat.end(),
+                mat.as_str().parse().unwrap(),
+            ) {
+                total += mat.as_str().parse::<i32>().unwrap();
+            }
+        }
+    }
+
+    println!("part 1: {}", total);
+
+    let mut rat_total = 0;
+    for (_, v) in gear_nums.iter() {
+        if v.len() == 2 {
+            rat_total += v[0] * v[1];
+        }
+    }
+
+    println!("part 2: {}", rat_total);
 }
 
 fn day_4() {
